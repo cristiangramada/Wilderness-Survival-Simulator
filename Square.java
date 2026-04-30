2d822203e1c5ecc7dbaf4e093e1757b91f181ed5
juliarias20 <arias.julie05@gmail.com>
2026-04-29 20:09:11 -0700

    Added generateBonus + randomization

--- Square.java
+++ Square.java
@@ -4,39 +4,72 @@ Description: This class represent a square space in a map. It connects the progr
 Variables:
     - int x: coordinate of the square in the x-axis
     - int y: coordinate of the square in the y-axis
+    - int bonus_prob: the probability of a bonus items being in the square
     - Terrain terrain_type: the type of terrain in the square
-    - Item[] items: the items that are in the square
-Methods:
+    - ArrayList<Item> items: array list ofthe items that are in the square
+    - Random rand: random number generator used to determine random events in the square, such as bonus items, and the type of bonus item.
+
+    Methods:
+    - Suare(int x, int y, Terrain terrain_type): constructor for the Square class
     - getItems(): returns the items in the square
     - getTerrainType(): returns the terrain type of the square
+    - setTerrain(String terrain): sets the terrain type of the square based on the string input
     - addItem(Item item): adds an item to the square
     - removeItem(Item item): removes an item from the square
+    - generateBonus(): generates bonus items in the square based on the bonus probability
 
 TODO: 
-    -- array list errors remove when Item class is implemented
-    -- test the add and remove item methods when Item class is implemented
-    -- implement array list of items, create method to generate items based on randomization?
+    1. Assign values to the items in the generateBonus() method
+    2. Determine bonus item limits per square?
+    3. Add the Item object onto the generateBonus method
 */
 
 import java.util.ArrayList;
+import java.util.Random;
 
 public class Square
 {
     private int x;
     private int y;
+    private int bonus_prob;
     private Terrain terrain_type;
     private ArrayList<Item> items = new ArrayList<>();
+    private Random rand = new Random();
 
-    public Square(int x, int y, Terrain terrain_type)
+    public Square(int x, int y)
     {
         this.x = x;
         this.y = y;
-        this.terrain_type = terrain_type;
+        this.bonus_prob = rand.nextInt(100) + 1;
+        if (bonus_prob >= 80)
+        {
+            generateBonus();
+        }
     }
 
     public void generateBonus()
     {
-        
+        // TODO: 1, 2, 3
+        // *might change num of items generated**
+       for(int i = 0; i < 3; i++)
+        {
+            int item_type = rand.nextInt(3) + 1;
+            switch (item_type)
+            {
+                case 1: 
+                    items.add();
+                    break;
+                case 2: 
+                    items.add();
+                    break;
+                case 3:
+                    items.add();
+                    break;
+                default:
+                    break;
+            }
+            
+        }
     }
 
     public ArrayList<Item> getItems() 
@@ -50,6 +83,32 @@ public class Square
 
     public Terrain getTerrainType() 
     {
+        return terrain_type;
+    }
+
+    public Terrain setTerrain(String terrain)
+    {
+        switch (terrain)
+        {
+            case "Plains":
+                terrain_type = new Plains();
+                break;
+            case "Forest":
+                terrain_type = new Forest();
+                break;
+            case "Mountain":
+                terrain_type = new Mountain();
+                break;
+            case "Swamp":
+                terrain_type = new Swamp();
+                break;
+            case "Desert":
+                terrain_type = new Desert();
+                break;
+            default:
+                break;
+        }
+        
         return terrain_type;
     }
 
